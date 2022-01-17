4ad4e639ca15723b5215cfb7680c9a477590ffaa
Vigor-Lundkvist <vigor.lundkvist@live.se>
2022-01-17 11:13:55 +0100

    Fetch task 2, completed.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,27 +1,45 @@
 'use strict'
 
-console.log('Hello from index!')
+//task 2
 
-let dataURL = '/javascripts/data.json';
-let req = new XMLHttpRequest();
-req.open('GET', dataURL); //preppa för att gå och  hämta data
-req.responseType='json'; //preppa för att ta emot json
-req.send(); //go fetch!
+console.log('Hello from index!');
 
-console.log('data received');
+const fetchNames = fetch('/data')
+    .then((response) => response.json()
+    .then(res => Load(res)));
 
-//preppa vad som ska hända, när hämtningen är klar
-//dvs när all data har hämtats, då triggas onload eventet
-//och här skriver vi vad som ska ske då
-req.onload = () => {
-    const data = req.response;
-    console.log(data); 
-    
+const Load = (res) => {
+    console.log(res[0]);
 
-    data.forEach(element => {
+    res.forEach((element => {
         const li = document.createElement('li');
         li.textContent = element;
         document.getElementById('list').appendChild(li);
-    });
-   
+    }));
 }
+
+// task 1
+
+// let dataURL = '/javascripts/data.json';
+// let req = new XMLHttpRequest();
+// req.open('GET', dataURL); //preppa för att gå och  hämta data
+// req.responseType='json'; //preppa för att ta emot json
+// req.send(); //go fetch!
+
+// console.log('data received');
+
+// //preppa vad som ska hända, när hämtningen är klar
+// //dvs när all data har hämtats, då triggas onload eventet
+// //och här skriver vi vad som ska ske då
+// req.onload = () => {
+//     const data = req.response;
+//     console.log(data); 
+    
+
+//     data.forEach(element => {
+//         const li = document.createElement('li');
+//         li.textContent = element;
+//         document.getElementById('list').appendChild(li);
+//     });
+   
+//}
